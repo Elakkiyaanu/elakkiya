32b88b4d5e197e154c228b4e52af90f211c60d9e
Elakkiyaanu <elakkiyalea@gmail.com>
2018-05-29 12:28:00 +0530

    basics

---
+++ Helloword.java
@@ -0,0 +1,12 @@
+public class Helloword
+{
+public static void main(String s[])
+{
+System.out.println("Welcome to java training!!!");
+byte one=100;
+byte two=20;
+int three=one+two;
+System.out.println("three");
+
+}
+}
